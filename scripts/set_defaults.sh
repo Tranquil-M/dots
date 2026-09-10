5827f2921cfda784eb568596cc8503920ffad265
Tranquil-M <bpihwhshmh@gmail.com>
2026-09-09 20:16:24 -0700

    added more packages

--- scripts/set_defaults.sh
+++ scripts/set_defaults.sh
@@ -9,6 +9,8 @@ xdg-mime default firefox.desktop x-scheme-handler/http
 xdg-mime default firefox.desktop x-scheme-handler/https
 sudo systemctl enable --now greetd
 sudo systemctl enable --now power-profiles-daemon.service
+sudo systemctl --user restart xdg-desktop-portal.service
+sudo systemctl --user restart xdg-desktop-portal-termfilechooser.service
 sudo usermod -a -G input $USER
 
 for mime in text/plain text/markdown text/x-tex text/html text/css text/javascript \
