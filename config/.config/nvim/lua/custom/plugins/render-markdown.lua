d4c47a10e21d1094b603400fe6b103adefc8bb7e
Tranquil-M <bpihwhshmh@gmail.com>
2026-09-10 10:25:04 -0700

    removed markdown render plugin

--- config/.config/nvim/lua/custom/plugins/render-markdown.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- or nvim-web-devicons
-    opts = {
-        enabled = true,
-        render_modes = { 'n', 'c', 't' }, -- Modes where rendering is active (Normal, Command, Terminal)
-    },
-}
