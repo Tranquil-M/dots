7948fee3e32a1dfd3d67101577b059b99dc1970e
Tranquil-M <bpihwhshmh@gmail.com>
2026-09-10 15:53:42 -0700

    moved todo highlighting

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -166,17 +166,6 @@ local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- Highlight todo, notes, etc in comments
-  {
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    ---@module 'todo-comments'
-    ---@type TodoOptions
-    ---@diagnostic disable-next-line: missing-fields
-    opts = { signs = false },
-  },
-
   { -- Collection of various small independent plugins/modules
     'nvim-mini/mini.nvim',
     config = function()
--- config/.config/nvim/lua/plugins/editorenv.lua
+++ config/.config/nvim/lua/plugins/editorenv.lua
@@ -38,4 +38,14 @@ return {
       },
     },
   },
+
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    ---@module 'todo-comments'
+    ---@type TodoOptions
+    ---@diagnostic disable-next-line: missing-fields
+    opts = { signs = false },
+  },
 }
