ad3b405a40a56ff4ea3cf4d1457ebe0b5e6d8527
Tranquil-M <bpihwhshmh@gmail.com>
2026-09-10 13:28:38 -0700

    remove empty buffers

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -135,6 +135,32 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.hl.on_yank() end,
 })
 
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function(args)
+    local current = args.buf
+
+    -- Only run for actual files
+    if vim.api.nvim_buf_get_name(current) == "" then
+      return
+    end
+
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
+        local name = vim.api.nvim_buf_get_name(buf)
+        local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+        if name == ""
+          and #lines == 1
+          and lines[1] == ""
+          and not vim.bo[buf].modified
+        then
+          vim.api.nvim_buf_delete(buf, { force = true })
+        end
+      end
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
--- config/.config/nvim/lua/custom/plugins/neotree.lua
+++ config/.config/nvim/lua/custom/plugins/neotree.lua
@@ -3,14 +3,12 @@ return {
   version = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- optional, but highly recommended for file icons
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    "3rd/image.nvim",
   },
   config = function()
-    -- If you want custom configurations, they go here
     require("neo-tree").setup({
-      -- your configuration options
     })
   end
 }
