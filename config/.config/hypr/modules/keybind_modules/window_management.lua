05dbfd827c293b42110db562366b0cab82b7baaa
Tranquil-M <bpihwhshmh@gmail.com>
2026-09-10 19:52:30 -0700

    change arrow keys or hjkl for moving windows

--- config/.config/hypr/modules/keybind_modules/window_management.lua
+++ config/.config/hypr/modules/keybind_modules/window_management.lua
@@ -2,17 +2,17 @@
 local mainMod = "SUPER"
 
 -- Window Focus
-hl.bind(mainMod .. " + LEFT",  hl.dsp.focus({ direction = "l" }))
-hl.bind(mainMod .. " + RIGHT", hl.dsp.focus({ direction = "r" }))
-hl.bind(mainMod .. " + UP",    hl.dsp.focus({ direction = "u" }))
-hl.bind(mainMod .. " + DOWN",  hl.dsp.focus({ direction = "d" }))
+hl.bind(mainMod .. " + h", hl.dsp.focus({ direction = "l" }))
+hl.bind(mainMod .. " + l", hl.dsp.focus({ direction = "r" }))
+hl.bind(mainMod .. " + j", hl.dsp.focus({ direction = "u" }))
+hl.bind(mainMod .. " + k", hl.dsp.focus({ direction = "d" }))
 
 -- Window Movement
-hl.bind(mainMod .. " + SHIFT + LEFT",  hl.dsp.window.move({direction = "l" }))
-hl.bind(mainMod .. " + SHIFT + RIGHT", hl.dsp.window.move({direction = "r" }))
-hl.bind(mainMod .. " + SHIFT + UP",    hl.dsp.window.move({ direction = "u" }))
-hl.bind(mainMod .. " + SHIFT + DOWN",  hl.dsp.window.move({ direction = "d" }))
-hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
+hl.bind(mainMod .. " + SHIFT + h", hl.dsp.window.move({ direction = "l" }))
+hl.bind(mainMod .. " + SHIFT + l", hl.dsp.window.move({ direction = "r" }))
+hl.bind(mainMod .. " + SHIFT + k", hl.dsp.window.move({ direction = "u" }))
+hl.bind(mainMod .. " + SHIFT + j", hl.dsp.window.move({ direction = "d" }))
+hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Actions
@@ -20,7 +20,6 @@ hl.bind(mainMod .. " + W", hl.dsp.window.close())
 hl.bind(mainMod .. " + SHIFT + W", hl.dsp.window.kill())
 
 hl.bind(mainMod .. " + T", function()
-
 	local monitor = hl.get_active_monitor()
 	local window = hl.get_active_window()
 
@@ -28,11 +27,9 @@ hl.bind(mainMod .. " + T", function()
 		hl.dispatch(hl.dsp.window.float({ action = "toggle" }))
 		hl.dispatch(hl.dsp.window.center())
 		if window.floating then
-			hl.dispatch(hl.dsp.window.resize({ x = (monitor.width*0.4), y = (monitor.height*0.5) }))
+			hl.dispatch(hl.dsp.window.resize({ x = (monitor.width * 0.4), y = (monitor.height * 0.5) }))
 		end
 	end
-
-
 end)
 
 hl.bind(mainMod .. " + ALT + F", hl.dsp.window.fullscreen({ mode = "fullscreen" }))
