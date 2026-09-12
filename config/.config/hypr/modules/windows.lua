f920b2cf698d3276b02a445319b11342d8e22d9e
Tranquil-M <bpihwhshmh@gmail.com>
2026-09-11 17:30:58 -0700

    hopefully maybe fixed pip sizing and location

--- config/.config/hypr/modules/windows.lua
+++ config/.config/hypr/modules/windows.lua
@@ -1,93 +1,83 @@
 hl.window_rule({
-    -- Ignore maximize requests from all apps. You'll probably like this.
-    name = "suppress-maximize-events",
-    match = { class = ".*" },
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
 
-    suppress_event = "maximize",
+	suppress_event = "maximize",
 })
 
 hl.window_rule({
-    -- Fix some dragging issues with XWayland
-    name  = "fix-xwayland-drags",
-    match = {
-        class      = "^$",
-        title      = "^$",
-        xwayland   = true,
-        float      = true,
-        fullscreen = false,
-        pin        = false,
-    },
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
 
-    no_focus = true,
+	no_focus = true,
 })
 
 hl.window_rule({
-    name  = "move-hyprland-run",
-    match = { class = "hyprland-run" },
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
 
-    move  = "20 monitor_h-120",
-    float = true,
+	move = "20 monitor_h-120",
+	float = true,
 })
 
 hl.window_rule({
-    name  = "satty-always-floating",
-    match = { class = "^(com.gabm.satty)" },
+	name = "satty-always-floating",
+	match = { class = "^(com.gabm.satty)" },
 
-    float = true,
-    size = {800, 600},
-    center = true,
+	float = true,
+	size = { 800, 600 },
+	center = true,
 })
 
 hl.window_rule({
-    name  = "blueman-always-floating",
-    match = { class = "^(blueman-manager)" },
+	name = "blueman-always-floating",
+	match = { class = "^(blueman-manager)" },
 
-    float = true,
-    size = {1000, 600},
-    center = true,
+	float = true,
+	size = { 1000, 600 },
+	center = true,
 })
-
--- hl.layer_rule({
---     name = "noctalia",
---     match = {
---         namespace = "noctalia-background-.*$"
---     },
---     ignore_alpha = 0.5,
---     blur = true,
---     blur_popups = true
--- })
 
 hl.config({
-    xwayland = {
-        force_zero_scaling = true
-    }
+	xwayland = {
+		force_zero_scaling = true,
+	},
 })
 
 hl.window_rule({
-    match = { class = "^(discord)$" },
-    render_unfocused = true
+	match = { class = "^(discord)$" },
+	render_unfocused = true,
 })
 
 hl.window_rule({
-  match = {
-    title = "^(Picture-in-Picture|Picture in picture|Picture-In-Picture)$",
-  },
+	match = {
+		title = "^(Picture-in-Picture|Picture in picture|Picture-In-Picture)$",
+	},
 
-  float = true,
-  pin = true,
+	float = true,
+	pin = true,
 
-  size = { "monitor_w * 0.25", "monitor_h * 0.25" },
-  move = { "(monitor_w - (window_w * 0.1))", "(monitor_h - (window_h * 0.25))" },
+	size = { "monitor_w * 0.25", "monitor_h * 0.25" },
+	move = { "(monitor_w - (window_w * 1.1))", "(monitor_h - (window_h * 1.1))" },
 
-  animation = "slide right",
+	animation = "slide right",
 })
 
 hl.window_rule({
-    match = {
-        title = "Noctalia Settings",
-    },
+	match = {
+		title = "Noctalia Settings",
+	},
 
-    float = true,
-    center = true,
-    size = { "monitor_w * 0.5", "monitor_h * 0.8" },
+	float = true,
+	center = true,
+	size = { "monitor_w * 0.5", "monitor_h * 0.8" },
 })
