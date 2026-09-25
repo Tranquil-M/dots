c9ccd4a13a23db5461d189b3e5013c9ab3c6ffb6
Tranquil-M <bpihwhshmh@gmail.com>
2026-09-24 17:26:18 -0700

    improved basic screenshot functionality with sfx and grimblast

--- config/.config/hypr/modules/keybind_modules/screenshots.lua
+++ config/.config/hypr/modules/keybind_modules/screenshots.lua
@@ -3,6 +3,20 @@ local mainMod = "SUPER"
 local ipc = "noctalia msg"
 
 -- Execution
-hl.bind(mainMod .. " + z", hl.dsp.exec_cmd(ipc .. " plugin alexander/screen-toolkit:service all annotate"))
+hl.bind(mainMod .. " + z", function()
+	local file_path = os.getenv("HOME") .. "/Pictures/Screenshots/Screenshot_" .. os.date("%Y-%m-%d_%H-%M-%S") .. ".png"
 
-hl.bind(mainMod .. " + SHIFT + z", hl.dsp.exec_cmd(ipc .. " plugin alexander/screen-toolkit:service all annotateFullscreen"))
+	local cmd = "grimblast save area "
+		.. file_path
+		.. " && mpv "
+		.. os.getenv("HOME")
+		.. "/.config/hypr/sfx/camera-shutter.mp3 && notify-send 'Screenshot Taken' 'Saved to Pictures' -i "
+		.. file_path
+
+	hl.dispatch(hl.dsp.exec_cmd(cmd))
+end)
+
+hl.bind(
+	mainMod .. " + SHIFT + z",
+	hl.dsp.exec_cmd(ipc .. " plugin alexander/screen-toolkit:service all annotateFullscreen")
+)
